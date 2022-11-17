8877f811b4c27450d8812c7119407052e8a8322d
unknown <steveweed1979@gmail.com>
2022-11-17 14:35:56 -0600

    Fleshing out the README

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
+//TODO:  Uncomment when routes are in place
 // const routes = require('./routes');
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
+//TODO: Will need to uncomment when routes are in place
 // app.use(routes);
 
 db.once("open", () => {
