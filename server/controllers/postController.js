3fd9b7157c7515c836f138077022e2f6209ac34b
Gavin <gavinporlich@gmail.com>
2022-11-17 20:56:58 -0600

    getPostById function made

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -10,3 +10,13 @@ async function getAllPosts(req, res) {
     res.status(500).json(err);
   };
 };
+
+async function getPostById(req, res) {
+    try {
+        const singlePost = await Post.findById(req.params.id).select('-__v');
+        res.status(200).json(singlePost);
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
+    };
+};
