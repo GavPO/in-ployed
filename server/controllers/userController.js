47ed0854e038898081c93a1978d6abfce719ae09
Gavin <gavinporlich@gmail.com>
2022-11-17 19:52:25 -0600

    getUserById function made

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,9 +6,23 @@ async function getAllUsers(req, res) {
         const allUsers = await User.find()
         .select('-__v')
         .populate('posts');
-        res.status(200).json(allUsers)
-    } catch (err) {
+        res.status(200).json(allUsers);
+    } catch (err) {;
         console.error(err)
-        res.status(500).json(err)
-    }
+        res.status(500).json(err);
+    };
+};
+
+async function getUserById(req, res) {
+    try {
+        const singleUser = await User.findById(req.params.userId)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    };
+};
+
+module.exports = {
+    getAllUsers,
+    getUserById
 };
