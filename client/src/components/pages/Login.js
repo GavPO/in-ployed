69f528ab083619c98b217c05ede0a2496d2a9ee2
Johnny <johnathan.mnn@gmail.com>
2022-11-21 15:27:05 -0600

    Added login/signup

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import '../../styles/Login.css';
 
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <div className="row" id='login'>
+  <div className="col-md-6 registration">
+    <h2>Login</h2>
+
+    <form className="form login-form">
+      <div className="form-group">
+        <label for="email-login">Email:</label><br/>
+        <input className="form-input" type="text" id="email-login" />
+      </div>
+      <div className="form-group">
+        <label for="password-login">Password:</label><br/>
+        <input className="form-input" type="password" id="password-login" />
+      </div>
+      <div className="form-group">
+        <button className="btn" type="submit">Login</button>
+      </div>
+    </form>
+  </div>
+  <div className="col-md-6 registration">
+    <h2>Signup</h2>
+
+    <form className="form signup-form">
+      <div className="form-group">
+        <label for="name-signup">Name:</label><br/>
+        <input className="form-input" type="text" id="name-signup" />
+      </div>
+      <div className="form-group">
+        <label for="email-signup">Email:</label><br/>
+        <input className="form-input" type="text" id="email-signup" />
+      </div>
+      <div className="form-group">
+        <label for="password-signup">Password:</label><br/>
+        <input className="form-input" type="password" id="password-signup" />
+      </div>
+      <div className="form-group">
+        <button className="btn" type="submit">Signup</button>
+      </div>
+    </form>
+  </div>
+</div>
   );
 }
