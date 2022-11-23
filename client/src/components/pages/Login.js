08810dc7e2547c266cfb65c839cff386b6fc63e7
SteveWeed79 <steveweed1979@gmail.com>
2022-11-22 19:27:43 -0600

    Auth added to utils

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,47 +1,62 @@
-import React from 'react';
-import '../../styles/Login.css';
+import React from "react";
+import "../../styles/Login.css";
+
+import Auth from "../utils/auth";
 
 export default function Login() {
   return (
-    <div className="row" id='login'>
-  <div className="col-md-6 registration">
-    <h2>Login</h2>
+    <div className="row" id="login">
+      <div className="col-md-6 registration">
+        <h2>Login</h2>
 
-    <form className="form login-form">
-      <div className="form-group">
-        <label htmlFor="email-login">Email:</label><br/>
-        <input className="form-input" type="text" id="email-login" />
+        <form className="form login-form">
+          <div className="form-group">
+            <label htmlFor="email-login">Email:</label>
+            <br />
+            <input className="form-input" type="text" id="email-login" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-login">Password:</label>
+            <br />
+            <input className="form-input" type="password" id="password-login" />
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="form-group">
-        <label htmlFor="password-login">Password:</label><br/>
-        <input className="form-input" type="password" id="password-login" />
-      </div>
-      <div className="form-group">
-        <button className="btn" type="submit">Login</button>
-      </div>
-    </form>
-  </div>
-  <div className="col-md-6 registration">
-    <h2>Signup</h2>
+      <div className="col-md-6 registration">
+        <h2>Signup</h2>
 
-    <form className="form signup-form">
-      <div className="form-group">
-        <label htmlFor="name-signup">Name:</label><br/>
-        <input className="form-input" type="text" id="name-signup" />
+        <form className="form signup-form">
+          <div className="form-group">
+            <label htmlFor="name-signup">Name:</label>
+            <br />
+            <input className="form-input" type="text" id="name-signup" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email-signup">Email:</label>
+            <br />
+            <input className="form-input" type="text" id="email-signup" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-signup">Password:</label>
+            <br />
+            <input
+              className="form-input"
+              type="password"
+              id="password-signup"
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">
+              Signup
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="form-group">
-        <label htmlFor="email-signup">Email:</label><br/>
-        <input className="form-input" type="text" id="email-signup" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password-signup">Password:</label><br/>
-        <input className="form-input" type="password" id="password-signup" />
-      </div>
-      <div className="form-group">
-        <button className="btn" type="submit">Signup</button>
-      </div>
-    </form>
-  </div>
-</div>
+    </div>
   );
 }
--- client/src/components/pages/Post.js
+++ client/src/components/pages/Post.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import '../../styles/Post.css';
+import React from "react";
+import "../../styles/Post.css";
 
+import Auth from "../utils/auth";
 
 export default function Post() {
-  return(
+  return (
     <div className="container-fluid" id="createPost">
       <h1>New Post</h1>
       <form>
-      <div className="form-group">
-        <h3><label htmlFor="code">Post Title:</label></h3><br/>
-        <textarea className="form-control" id="postTitle" rows="1"></textarea>
-      </div>
-      <div className="form-group">
-        <h3><label htmlFor="code">Your Code:</label></h3><br/>
-        <textarea className="form-control" id="postContent" rows="3"></textarea>
-      </div>
-      <div className="form-group">
-        <button className="btn" type="submit">Submit</button>
-      </div>
+        <div className="form-group">
+          <h3>
+            <label htmlFor="code">Post Title:</label>
+          </h3>
+          <br />
+          <textarea className="form-control" id="postTitle" rows="1"></textarea>
+        </div>
+        <div className="form-group">
+          <h3>
+            <label htmlFor="code">Your Code:</label>
+          </h3>
+          <br />
+          <textarea
+            className="form-control"
+            id="postContent"
+            rows="3"
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
-  )
+  );
 }
