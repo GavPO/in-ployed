b48dd7c28cc3880e65b58beab65ecf5d450d964c
Gavin <gavinporlich@gmail.com>
2022-11-28 21:34:52 -0600

    fixed wrong route

--- server/routes/api/postRoutes.js
+++ server/routes/api/postRoutes.js
@@ -8,13 +8,13 @@ const {
     upvotePost,
 } = require('../../controllers/postController');
 
-// /api/posts
+// /api/posts/
 router.route('/').get(getAllPosts).post(createPost);
 
-// /api/Posts/:userId
-router.route('/:postId').get(getPostById).put(updatePost).delete(deletePost);;
+// /api/Posts/postId
+router.route('/:postId').get(getPostById).put(updatePost).delete(deletePost);
 
-// /api/Posts/PostId
-router.route('/upvotes/:postId').put(upvotePost)
+// /api/Posts/upvotes
+router.route('/upvotes/:postId/:userId').put(upvotePost);
 
 module.exports = router;
